b8d4fc220d1e186f33ab98adc912c67f6e93952e
Tsar-dev <aziz,u@students.amikom.ac.id>
2020-12-19 12:07:06 +0700

    modified title

--- konfirmasi.php
+++ konfirmasi.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Rumah Sakit 2</title>
+    <title>RSJ</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alatsi">
