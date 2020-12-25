aed2aff38f1e870b29e6002a0437344e1e1fc943
Tsar-dev <aziz,u@students.amikom.ac.id>
2020-12-25 18:37:03 +0700

    rename css

--- landing.php
+++ landing.php
@@ -40,7 +40,7 @@ if (isset($_POST["daftar"]))
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="assets/css/Footer-Basic.css">
     <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/styless.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
