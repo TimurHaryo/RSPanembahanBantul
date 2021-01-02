3b70ce13a02c23fb765f3c5edb4e9ee16cc68702
TimurHaryo <timurharyo00@gmail.com>
2021-01-02 13:36:31 +0700

    tidy up

--- process_simpan.php
+++ process_simpan.php
@@ -3,10 +3,12 @@ session_start();
 $id_patient = $_SESSION['norm'];
 include_once("connection.php");
 $a = 0;
+
 for ($i = 0; $i<6; $i++) 
 {
     $a .= mt_rand(0,9);
 }
+
 $ab = strval($a); 
 $aa = "RS-";
 $kode = $aa.$ab;
@@ -18,8 +20,9 @@ $queryS = "SELECT id_clinic_schedule FROM clinic_schedule WHERE id_clinic = '$id
 $resultS = mysqli_query($koneksi->connect, $queryS);
 $row = mysqli_fetch_assoc($resultS);
 $id_clinic_scheduling = $row['id_clinic_schedule'];
+
 echo $id_clinic_scheduling;
-//$_SESSION['ses_time'] = $id_clinic_scheduling;
+
 $_SESSION['kode_boking'] = $kode;
 $query = "INSERT INTO booking (id_booking, id_patient, id_clinic_scheduling) VALUES ('$kode','$id_patient',$id_clinic_scheduling)";
 $result = mysqli_query($koneksi->connect, $query);
@@ -32,23 +35,16 @@ if ($query == TRUE) {
 
 $name_clinic = $_SESSION['ses_policlinic'];
 $query2 = "SELECT booking.id_booking, clinic.name_clinic, clinic.queue
-FROM booking INNER JOIN clinic_schedule
-ON booking.id_clinic_scheduling=clinic_schedule.id_clinic_schedule
-INNER JOIN clinic
-ON clinic_schedule.id_clinic=clinic.id_clinic
-WHERE clinic.id_clinic = '$name_clinic'";
-$result2 = mysqli_query($koneksi->connect, $query2);
-//$no = 1;
-//$query2 = "UPDATE clinic SET queue = 1 WHERE name_clinic = '$name_clinic'";
-//mysqli_query($koneksi->connect, $query2);
+        FROM booking INNER JOIN clinic_schedule
+        ON booking.id_clinic_scheduling=clinic_schedule.id_clinic_schedule
+        INNER JOIN clinic
+        ON clinic_schedule.id_clinic=clinic.id_clinic
+        WHERE clinic.id_clinic = '$name_clinic'";
 
-// proses updating id dengan nilai $no
+$result2 = mysqli_query($koneksi->connect, $query2);
 $query3 = "UPDATE clinic SET queue = queue+1 WHERE id_clinic = '$ide_clinic'";
 mysqli_query($koneksi->connect, $query3);
 
-// mengubah nilai auto increment menjadi $no terakhir ditambah 1
-// $query2 = "ALTER TABLE clinic AUTO_INCREMENT = $no";
-// mysqli_query($koneksi->connect, $query2);
 if ($query2 == TRUE) {
     echo " Queue created successfully";
   } else {
